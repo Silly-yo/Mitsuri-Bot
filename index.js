6d7375d2994cdf820d965bb354659b6395677493
Silly <148607281+Silly-yo@users.noreply.github.com>
2024-05-03 19:47:56 +0530

    index.js

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ client.once('ready', () => {
   setTimeout(() => {
     console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
     client.user.setPresence({
-      activities: [{ name: `Update Out V@1.5`, type: ActivityType.Watching }],
+      activities: [{ name: `Silly❤️`, type: ActivityType.Playing }],
       status: 'idle',
     });
   }, 2000); 
