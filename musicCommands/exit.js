bee69cb39f550e40291748161072431f3d644418
Silly <148607281+Silly-yo@users.noreply.github.com>
2024-05-03 20:05:18 +0530

    Update exit.js

--- musicCommands/exit.js
+++ musicCommands/exit.js
@@ -1,1 +1,15 @@
+const { VoiceConnectionStatus } = require('@discordjs/voice');
+const playModule = require('./play.js');
 
+module.exports = {
+  name: 'exit',
+  description: 'disconnect the music bot',
+  execute: (message, args) => {
+    const currentConnection = playModule.getCurrentConnection();
+    if (currentConnection && currentConnection.state.status === VoiceConnectionStatus.Ready) {
+      playModule.pause();
+    } else {
+      message.reply('❌ The bot is not currently playing any music.');
+    }
+  },
+};
